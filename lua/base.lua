7dbb113eabc53b22eef78e710781de62c12a9267
katinbox <tranquanki28@gmail.com>
2022-08-30 16:44:32 +0700

    base configuration

--- lua/base.lua
+++ lua/base.lua
@@ -9,3 +9,35 @@ vim.wo.number = true
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = 'fish'
+vim.opt.backupskip = 'tmp/*, private/tmp/*'
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ai = true -- auto indent
+vim.opt.si = true --smart indent
+vim.opt.wrap = false -- no wrap lines
+vim.opt.backspace = 'start,eol,indent'
+vim.opt.path:append {'**'} -- Finding files - Search down into subfolders
+vim.opt.wildignore:append {'*/node_modules/*'}
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Cs = "\e[4:0m"]])
+
+-- Turn off paste mode on living insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = 'set nopaste',
+})
+
+vim.opt.formatoptions:append {'r'}
